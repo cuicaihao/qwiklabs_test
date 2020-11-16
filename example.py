73354edc3dbaa3d77f2d8af314ebfe9140fe151a
Chris (Caihao) Cui <cuicaihao@users.noreply.github.com>
2020-11-16 13:29:41 +1100

    add header to the example.py file

--- example.py
+++ example.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+Created on   :2020/11/16 13:29:07
+@author      :Caihao (Chris) Cui
+@file        :example.py
+@content     :xxx xxx xxx
+@version     :0.1
+@License :   (C)Copyright 2020 MIT
+'''
+
+# here put the import lib
+
+
 def git_operation():
     print("I amm adding example.py file to the remote repository.")
 
-git_operation()
 
+git_operation()
